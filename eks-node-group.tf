752ab8d332ef6d82269821401a6c2d63e90a9c64
hariramprasanth <hariramprasanth0@gmail.com>
2023-04-24 12:29:56 +0530

    Add node group

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -1,19 +1,33 @@
-# resource "aws_eks_node_group" "eks_node_group" {
-#   cluster_name = aws_eks_cluster.my_web_server_eks.name
-#   node_group_name = "web-server-cluster-node-group"
-#   node_role_arn = aws_iam_role.eks_cluster_node_role.arn
-#   subnet_ids = [
-#     aws_subnet.my_subnet_test_1.id ,
-#     aws_subnet.my_subnet_test_2.id ,
-#     aws_subnet.my_subnet_test_3.id 
-#     ]
-#     depends_on = [
-#       aws_iam_role_policy_attachment.eks_AmazonEC2ContainerRegistryReadOnly,
-#       aws_iam_role_policy_attachment.eks_AmazonEKS_CNI_Policy,
-#       aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy
-#     ]
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.my_web_server_eks.name
+  node_group_name = "web-server-cluster-node-group"
+  node_role_arn   = aws_iam_role.eks_cluster_node_role.arn
+  ami_type = "AL2_x86_64"
+  capacity_type = "ON_DEMAND"
+  instance_types = [ "t2.small" ]
 
-# }
+  remote_access {
+    ec2_ssh_key = "my-test-ssh-key-pair"
+    source_security_group_ids = [ aws_security_group.my_sg_ssh.id ]
+  }
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+  subnet_ids = [
+    aws_subnet.my_subnet_test_1.id,
+    aws_subnet.my_subnet_test_2.id,
+    aws_subnet.my_subnet_test_3.id
+  ]
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.eks_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy
+  ]
+
+}
 
 resource "aws_iam_role" "eks_cluster_node_role" {
   name = "eks-cluster-nore-role"
